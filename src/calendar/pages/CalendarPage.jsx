262ad94dd5b273d68ee10ac24e4c2559ed94d916
Jahir Alberto Samperio <jahirsampe1234@gmail.com>
2024-09-24 11:10:56 -0600

    Creando login, registro y calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,7 +1,77 @@
-import React from 'react'
+import { Calendar } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { addHours } from 'date-fns'
+import { Navbar, CalendarEvent } from '../'
+
+import { localizer, getMessagesES } from '../../helpers';
+import { useState } from 'react';
+
+const events = [{
+  title: 'Cumpleaños del jefe',
+  notes: 'Hay que comprar pastel',
+  start: new Date(),
+  end: addHours( new Date(), 2 ),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Samperio'
+  }
+}]
 
 export const CalendarPage = () => {
+
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week')
+
+
+  //Le da estilos al evento seleccionado
+  const eventStyleGetter = (event, start, end, isSelected) => {    
+    const style = {
+      backgroundColor: '#327CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+  }
+
+  const onDoubleClick = (event) => {
+    console.log({doubleClick: event})
+  }
+
+  const onSelect = (event) => {
+    console.log({click: event})
+  }
+
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastView', event);
+  }
+
   return (
-    <div>CalendarPage</div>
+    <>
+      <Navbar />
+
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        defaultView={lastView}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px)' }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent
+        }}
+        onDoubleClickEvent={ onDoubleClick }
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
+      />
+
+    </>
   )
 }
